c9efaade01741d57a440183a8c37a504cb73ef94
Nisith Saranga <nisithsaranga13@gmail.com>
2026-02-26 04:44:52 +0530

    feat: add core utility helpers (json, validation, hashing)

--- src/main/java/com/oceanview/resort/util/JsonUtil.java
+++ src/main/java/com/oceanview/resort/util/JsonUtil.java
@@ -1,4 +1,38 @@
 package com.oceanview.resort.util;
 
-public class JsonUtil {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public final class JsonUtil {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper()
+            .findAndRegisterModules()
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    private JsonUtil() { }
+
+    public static ObjectMapper mapper() {
+        return MAPPER;
+    }
+
+    public static <T> T fromJson(InputStream inputStream, Class<T> clazz) throws IOException {
+        return MAPPER.readValue(inputStream, clazz);
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) throws IOException {
+        return MAPPER.readValue(json, clazz);
+    }
+
+    public static String toJson(Object value) throws JsonProcessingException {
+        return MAPPER.writeValueAsString(value);
+    }
+
+    public static void toJson(OutputStream outputStream, Object value) throws IOException {
+        MAPPER.writeValue(outputStream, value);
+    }
 }
